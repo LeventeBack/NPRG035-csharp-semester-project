cd54da3d5378ba742f101bbecadc7174bc88d530
Levente Back <backistvanlevente17@gmail.com>
2024-02-02 22:38:11 +0100

    fix navigation link

--- Src/Services/NavigatorService.cs
+++ Src/Services/NavigatorService.cs
@@ -21,7 +21,7 @@ class NavigatorService
 
     public void GoToHome() => NavigateTo("/");
     public void GoToSelectQuiz() => NavigateTo("/select-quiz");
-    public void GoToQuiz(int quizId) => NavigateTo($"/quiz/{quizId}");
+    public void GoToQuiz(int quizId) => NavigateTo($"/play-quiz/{quizId}");
     public void GoToManageQuizzes() => NavigateTo("/manage-quizzes");
     public void GoToManageQuestions(int quizId) => NavigateTo($"/manage-questions/{quizId}");
 }
